88c3a025fb3238a302a48a49dcb1f71432da3778
fearppen <antonchachylin@gmail.com>
2024-04-13 19:24:06 +0500

    Add CarRentContext

---
+++ Ion.Infrastructure/CarRentContext.cs
@@ -0,0 +1,21 @@
+using Ion.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ion.Infrastructure;
+
+public class CarRentContext : DbContext
+{
+    public DbSet<Announcement> Announcements { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<License> Licenses { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<TripRecord> TripRecords { get; set; }
+    public DbSet<User> Users { get; set; }
+    
+    public CarRentContext(DbContextOptions<CarRentContext> contextOptions) : base(contextOptions)
+    {
+        Database.EnsureCreated();
+    }
+}
