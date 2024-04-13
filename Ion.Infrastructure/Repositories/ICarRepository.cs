8a96c4960cd678977f35794e835e97441abfbb91
fearppen <antonchachylin@gmail.com>
2024-04-13 19:26:10 +0500

    Add UserRepository

--- Ion.Infrastructure/Repositories/ICarRepository.cs
+++ Ion.Infrastructure/Repositories/ICarRepository.cs
@@ -3,57 +3,8 @@ using Ion.Domain.Entities;
 
 namespace Ion.Infrastructure.Repositories;
 
-public class CarRepository(CarRentContext context) : ICarRepository
+public class CarRepository(CarRentContext context)
 {
     private readonly CarRentContext context = context;
 
-    public Task<Car> AddAsync(Car entity, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<Car>> AddRangeAsync(IEnumerable<Car> entities, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task UpdateAsync(Car entity, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task UpdateRangeAsync(IEnumerable<Car> entities, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DeleteAsync(Car entity, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DeleteRangeAsync(IEnumerable<Car> entities, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<Car> GetAll()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Car GetByID(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<Car>> GetByUserIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
 }
---
+++ Ion.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,6 @@
+namespace Ion.Infrastructure.Repositories;
+
+public class UserRepository(CarRentContext context)
+{
+    private readonly CarRentContext context;
+}
