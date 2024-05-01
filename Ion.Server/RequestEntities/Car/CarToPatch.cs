8d3765e57f26c1730cff04db72688e683d4ea660
S4MPAI <flekshum@gmail.com>
2024-05-01 21:41:39 +0500

    refactoring

--- Ion.Server/RequestEntities/Car/CarToPatch.cs
+++ Ion.Server/RequestEntities/Car/CarToPatch.cs
@@ -4,7 +4,7 @@ namespace Ion.Server.RequestEntities.Car;
 
 public class CarToPatch
 {
-    public int UserId { get; set; }
+    public int? UserId { get; set; }
     public GearboxType? GearboxType { get; set; }
     public string? Name { get; set; }
     public BodyType? BodyType { get; set; }
