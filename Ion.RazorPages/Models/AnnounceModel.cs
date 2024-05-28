b88c693acc45264c8ea44465ddeaaf878d2e271d
S4MPAI <flekshum@gmail.com>
2024-05-28 23:03:01 +0500

    updated Details in AnnouncementMVC

---
+++ Ion.RazorPages/Models/AnnounceModel.cs
@@ -0,0 +1,14 @@
+﻿using Ion.Server.RequestEntities.Announcement;
+using Ion.Server.RequestEntities.Review;
+using Ion.Server.RequestEntities.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ion.RazorPages.Models
+{
+    public class AnnounceModel
+    {
+        public UserToGet Author { get; set; }
+        public IEnumerable<ReviewToGet> Reviews { get; set; }
+        public AnnouncementToGet Annoncement { get; set; }
+    }
+}
