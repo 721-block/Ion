ccbac52902a18edbd6a971961383bc4c251e1d8a
fearppen <antonchachylin@gmail.com>
2024-05-29 16:10:49 +0500

    add UserImageService

--- Ion.Application/IServices/IUserImageService.cs
+++ Ion.Application/IServices/IUserImageService.cs
@@ -1,10 +1,9 @@
-﻿using Ion.Domain.Entities;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace Ion.Application.IServices;
 
 public interface IUserImageService
 {
-    public string ImagesString { get; set; }
+    public string ImagesPath { get; set; }
     public string UploadImages(IEnumerable<IFormFile> images, string userName, string carName);
 }
--- Ion.Application/Services/UserImageService.cs
+++ Ion.Application/Services/UserImageService.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ion.Application.IServices;
+using Microsoft.AspNetCore.Http;
 
-namespace Ion.Application.Services
+namespace Ion.Application.Services;
+
+public class UserImageService : IUserImageService
 {
-    public class UserImageService
+    public string ImagesPath { get; set; }
+    
+    public string UploadImages(IEnumerable<IFormFile> images, string userName, string carName)
     {
+        var pathToDirectory = Path.Combine(ImagesPath, userName, carName);
+        var fileIndex = 0;
+        foreach (var file in images)
+        {
+            if (file.Length <= 0) continue;
+            var filePath = Path.Combine(pathToDirectory, fileIndex.ToString());
+            using (var stream = new FileStream(filePath, FileMode.Create)) 
+            {
+                file.CopyTo(stream);
+            }
+
+            fileIndex++;
+        }
+
+        return pathToDirectory;
     }
 }
