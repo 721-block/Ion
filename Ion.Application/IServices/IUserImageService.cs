34c64d77cc8b14e4c63f7f6b1b9c8f768951683d
S4MPAI <flekshum@gmail.com>
2024-05-29 15:54:04 +0500

    added IUserImageService

--- Ion.Application/IServices/IUserImageService.cs
+++ Ion.Application/IServices/IUserImageService.cs
@@ -1,6 +1,10 @@
-﻿namespace Ion.Application.IServices;
+﻿using Ion.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Ion.Application.IServices;
 
 public interface IUserImageService
 {
-    public string UploadImages(IEnumerable<IFormFile>)
+    public string ImagesString { get; set; }
+    public string UploadImages(IEnumerable<IFormFile> images, string userName, string carName);
 }
