637a8cd00c114edb27317d306474cb17d70738d1
fearppen <antonchachylin@gmail.com>
2024-09-08 10:17:03 +0500

    Add models

---
+++ Ion.NewFront/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using Ion.Application.Base;
+using Ion.Server.RequestEntities;
+using Ion.Server.RequestEntities.Announcement;
+
+namespace Ion.RazorPages.Models
+{
+    public class IndexModel
+    {
+        public FilterParameters Parameters { get; set; } = new();
+        public IEnumerable<AnnouncementToGet> Announcements { get; set; }
+        public IEnumerable<string> Marks { get; set; }
+    }
+}
---
+++ Ion.NewFront/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using Ion.Server.RequestEntities.TripRecord;
+using Ion.Server.RequestEntities.User;
+
+namespace Ion.RazorPages.Models
+{
+    public class UserModel
+    {
+        public UserToGet UserToGet { get; set; }
+        public IEnumerable<TripRecordToGet> TripRecords { get; set; }
+    }
+}
