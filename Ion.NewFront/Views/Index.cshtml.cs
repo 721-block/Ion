61d45ff7bcab40416c3c555b049299ff430d4866
fearppen <antonchachylin@gmail.com>
2024-10-05 15:38:16 +0500

    Add viewmodels

---
+++ Ion.NewFront/Views/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Ion.Server.RequestEntities.Announcement;
+using Ion.Server.RequestEntities.Review;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ion.RazorPages.wwwroot.RazorPages;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
