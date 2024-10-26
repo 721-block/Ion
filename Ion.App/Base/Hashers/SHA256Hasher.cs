75503fa6ef801c42c9db2616f8c4f9ace55df338
fearppen <antonchachylin@gmail.com>
2024-10-26 15:38:16 +0500

    add services

---
+++ Ion.App/Base/Hashers/SHA256Hasher.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Ion.Application.Base.Hashers;
+
+public class SHA256Hasher : IHasher
+{
+    public string Hash(string input)
+    {
+        return Convert.ToHexString(SHA256.HashData(Encoding.ASCII.GetBytes(input)));
+    }
+}
