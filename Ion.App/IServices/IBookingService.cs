64dfea3e8dbcdc13be9f0c74af638ce0c96bfc9c
fearppen <antonchachylin@gmail.com>
2024-11-09 14:18:06 +0500

    append interfaces

---
+++ Ion.App/IServices/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using Ion.Application.ViewModels;
+
+namespace Ion.Application.IServices;
+
+public interface IBookingService
+{
+    IEnumerable<BookingViewModel> GetAll();
+    BookingViewModel GetById(int id);
+    IEnumerable<BookingViewModel> GetByAnnouncementId(int id);
+    IEnumerable<BookingViewModel> GetByClientId(int id);
+    IEnumerable<BookingViewModel> GetByAuthorId(int id);
+    Task<BookingViewModel> AddAsync(BookingViewModel model);
+    Task UpdateAsync(BookingViewModel model);
+    Task DeleteAsync(BookingViewModel model);
+    Task EndTripAsync(BookingViewModel model);
+}
---
+++ Ion.App/IServices/IUserImageService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ion.Application.IServices;
+
+public interface IUserImageService
+{
+    public string UploadImages(IEnumerable<IFormFile> images, int userId, int carId);
+}
