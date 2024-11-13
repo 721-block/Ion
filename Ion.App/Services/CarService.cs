f1d3e081c487c582dd15520e1bd9797b147206a5
S4MPAI <flekshum@gmail.com>
2024-11-13 17:23:01 +0500

    improve models

---
+++ Ion.App/Services/CarService.cs
@@ -0,0 +1,47 @@
+﻿using Ion.Application.IRepositories;
+using Ion.Application.IServices;
+using Ion.Application.ViewModels;
+using Ion.Domain.Entities;
+using MapsterMapper;
+
+namespace Ion.Application.Services;
+
+public class CarService(IMapper mapper, ICarRepository repository) : ICarService
+{
+    public async Task<CarViewModel> AddAsync(CarViewModel model)
+    {
+        var car = await repository.AddAsync(mapper.Map<Car>(model));
+        await repository.SaveChangesAsync();
+        return mapper.Map<CarViewModel>(car);
+    }
+
+    public async Task DeleteAsync(CarViewModel model)
+    {
+        repository.Delete(mapper.Map<Car>(model));
+        await repository.SaveChangesAsync();
+    }
+
+    public IEnumerable<CarViewModel> GetAll()
+    {
+        return repository.GetAll().Select(mapper.Map<CarViewModel>);
+    }
+
+    public CarViewModel GetById(int id)
+    {
+        return mapper.Map<CarViewModel>(repository.GetByID(id));
+    }
+
+    public IEnumerable<CarViewModel> GetByUserId(int id)
+    {
+        return repository.GetByUserId(id).Select(mapper.Map<CarViewModel>);
+    }
+
+    public async Task UpdateAsync(CarViewModel model)
+    {
+        var entity = repository.GetByID(model.Id);
+        var updatedEntity = mapper.Map(model, entity);
+
+        repository.Update(updatedEntity);
+        await repository.SaveChangesAsync();
+    }
+}
