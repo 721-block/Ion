4e5840c3d24be1ae2da404f0127aede97f96151a
HardreaM <zubkovandrej6@gmail.com>
2024-12-24 23:48:17 +0500

    Revert "append interfaces"
    This reverts commit 64dfea3e8dbcdc13be9f0c74af638ce0c96bfc9c.

--- Ion.App/IServices/IUserImageService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Ion.Application.IServices;
-
-public interface IUserImageService
-{
-    public string UploadImages(IEnumerable<IFormFile> images, int userId, int carId);
-}
