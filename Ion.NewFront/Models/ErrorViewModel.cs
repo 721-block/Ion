89d46d82833a979271f6ad8fefca4600f6113efe
HardreaM <zubkovandrej6@gmail.com>
2024-12-24 23:48:31 +0500

    Revert "Add models"
    This reverts commit 637a8cd00c114edb27317d306474cb17d70738d1.

--- Ion.NewFront/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ion.RazorPages.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
