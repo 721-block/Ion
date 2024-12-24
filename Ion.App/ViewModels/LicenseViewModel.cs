0210f543a672533723e0ac31a7584424a4b5e883
HardreaM <zubkovandrej6@gmail.com>
2024-12-24 23:48:14 +0500

    Revert "complete models"
    This reverts commit b8931791b714916a0b8e4b7073fcf5004e371ffc.

--- Ion.App/ViewModels/LicenseViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ion.Application.ViewModels;
-
-public class LicenseViewModel : BaseViewModel
-{
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-}
